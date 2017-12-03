bf6efdd0ff3d65f90813611c9d8dcfe253dd6951
wandrea <wagenhofferandrea@gmail.com>
2017-12-03 08:59:08 +0100

    #1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 
 public class Main {
     public static void main (String [] args){
+/*Deklaráltam két int típusú változót, amik a téglalap két oldalának a hosszát tárolják.
+ * Majd az int típusú area változóba kiszámoltam a téglalap területét, az int típusú perimeter változóba pedig a kerületét.
+  * Kiirattam konzolra a terület és kerület eredményeit.*/
+
+        int sideA =10;
+        int sideB=15;
+        int area = sideA *sideB;
+        int perimeter = (sideA + sideB)*2;
+
+        System.out.println("A téglalap területe: " + area + " cm^2 .\nA téglalap kerülete: " + perimeter + " cm");
 
     }
 }
